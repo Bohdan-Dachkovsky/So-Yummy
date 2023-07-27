2e5606f85c063a5d22e93816cca90b51a53ed4d1
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-07-27 10:26:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistedStore } from "./redux/store";
 import { Provider } from "react-redux";
-import Loader from "components/Loader/Loader";
+import Loader from "./components/Loader/Loader";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
