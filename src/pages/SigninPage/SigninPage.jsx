d70f64fa5ce77a6137f8223f9bfb17a56988011f
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-07-28 19:07:54 +0300

    Update SigninPage.jsx

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,11 +1,11 @@
-import { useSearchParams } from 'react-router-dom';
-import Signin from 'components/SignIn/SignIn';
-import { useDispatch } from 'react-redux';
-import { verify } from 'redux/auth/operations';
+import { useSearchParams } from "react-router-dom";
+import Signin from "components/SignIn/SignIn";
+import { useDispatch } from "react-redux";
+import { verify } from "../../redux/auth/operations";
 
 export default function SigninPage() {
   const [searchParams] = useSearchParams();
-  const verifyId = searchParams.get('verify');
+  const verifyId = searchParams.get("verify");
 
   const dispatch = useDispatch();
 
